46bfb86287981171b1fce6f4d7cde020168a9e6b
infected_ <zzitoz@gmail.com>
2023-01-29 10:54:04 +0000

    Initial PEX tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2019 The aospOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_instantnoodle.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_instantnoodle-user \
+    aosp_instantnoodle-userdebug \
+    aosp_instantnoodle-eng
